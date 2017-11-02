89512c456821873efb46de32b73c6105f1acf485
rjoy2 <rjoy@sapient.com>
2017-11-02 09:10:09 +0530

    Project structure changed from SapientRazorfish to FTD

--- src/main/java/com/ftd/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/com/ftd/eurekaservice/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sapientrazorfish.eurekaservice;
+package com.ftd.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
